03f5f3061d2161e02e05d6d662b108994ffe6e00
pakitowka <whitesaint12@gmail.com>
2023-06-02 17:58:11 +0200

    Main Window table with data displayed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QMainWindow, \
-    QLabel, QLineEdit, QPushButton, QTableWidget
+    QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, \
+    QDialog, QVBoxLayout
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -13,6 +15,7 @@ class MainWindow(QMainWindow):
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
         add_student_action = QAction("Add student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
@@ -20,13 +23,38 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        pass
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+
 
 
 app = QApplication(sys.argv)
 sm_system = MainWindow()
 sm_system.show()
+sm_system.load_data()
 sys.exit(app.exec())
